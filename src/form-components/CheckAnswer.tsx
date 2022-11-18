7ced0fd754d53460516aa1aaad199afa98137aad
brennanpoitras <brennan.poitras@maine.edu>
2022-11-18 12:31:37 -0500

    Finished checkAnswer component

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function checkRight(): JSX.Element {
+        if (answer === expectedAnswer) {
+            return <div>✔️</div>;
+        }
+        return <div>❌</div>;
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="formAnswer">
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control
+                    type="textbox"
+                    value={answer}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAnswer(event.target.value)
+                    }
+                />
+            </Form.Group>
+            <div>{checkRight()}</div>
         </div>
     );
 }
